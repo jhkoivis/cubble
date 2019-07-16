0fc44c98db48d0cb184913cc135c002af60c00e3
Evgeni Ulanov <evgeni.ulanov@aalto.fi>
2019-07-16 16:09:38 +0300

    Initial testing files - not yet working

---
+++ src/Main_tests.cpp
@@ -0,0 +1,11 @@
+#include "Util.h"
+#include <cuda.h>
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+int main(int argc, char **argv)
+{
+  std::cout << "run" << std::endl;
+}
---
+++ src/tests/main_test.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Evgeni Ulanov on 7/16/19.
+// Copyright (c) 2019
+//
+/*#include "gtest/gtest.h"
+
+int main(int argc, char **argv) {
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}*/
+
+int func(int x){ return x;}
