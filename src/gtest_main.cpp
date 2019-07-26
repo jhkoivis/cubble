949db589a690687bbad4b41b8c62b262e4e5e95e
Evgeni Ulanov <evgeni.ulanov@aalto.fi>
2019-07-26 12:07:12 +0300

    Simple working example of gtest

---
+++ src/gtest_main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Evgeni Ulanov on 7/16/19.
+// Copyright (c) 2019
+//
+#include "gtest/gtest.h"
+
+
+TEST(test_timesFive, integerTests){
+	EXPECT_EQ(5, 5);
+}
+
+int main(int argc, char* argv[]){
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
