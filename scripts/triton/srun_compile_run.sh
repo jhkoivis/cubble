f9027ad58353cded0c3c539c94ff522331cfc502
training003 Student <training003@puhti-login1.bullx>
2019-10-17 13:21:04 +0300

    Srun change

--- scripts/triton/srun_compile_run.sh
+++ scripts/triton/srun_compile_run.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-#SBATCH
-#SBATCH --job-name=
 #SBATCH --mem=10G
 #SBATCH --time=01:00:00
 #SBATCH --gres=gpu:v100:1
 #SBATCH --account=project_2001889
-#SBATCH --reservation=openACC_course_tue
+#SBATCH --reservation=openACC_course_thu
 
 module load gcc/8.3.0 cuda/10.1.168
 
-srun make -C final
+make -C final
 srun make -C run
